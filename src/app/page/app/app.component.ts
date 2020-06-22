aa8d82b1be71de4b20683cb5fad5019198d60752
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-06-22 15:52:37 -0300

    - Fixed to show login name

--- src/app/page/app/app.component.ts
+++ src/app/page/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@app/_models';
+import { first } from 'rxjs/operators';
+import { UserService } from '@app/_services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Shio CMS';
+  loading = false;
+  users: User[];
+  
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.loading = true;
+    this.userService.getAll().pipe(first()).subscribe(users => {
+      this.loading = false;
+      this.users = users;
+    });
+  }
+
 }
