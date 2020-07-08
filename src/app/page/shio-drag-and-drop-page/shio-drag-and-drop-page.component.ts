a7ade257966d992492974816a81fedf2bc043219
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-07-08 10:10:20 -0300

    - Fixed vscode experimentalDecorators

--- src/app/page/shio-drag-and-drop-page/shio-drag-and-drop-page.component.ts
+++ src/app/page/shio-drag-and-drop-page/shio-drag-and-drop-page.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shio-drag-and-drop-page',
-  templateUrl: './shio-drag-and-drop-page.component.html',
-  styleUrls: ['./shio-drag-and-drop-page.component.scss']
+  templateUrl: './shio-drag-and-drop-page.component.html'
 })
 export class ShioDragAndDropPageComponent implements OnInit {
 
